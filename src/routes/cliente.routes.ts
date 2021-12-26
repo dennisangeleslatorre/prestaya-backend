9753ed03a8a788a09880b3952150cb00cb8c6800
Dennis Angeles <denisarmando203@gmail.com>
2021-12-26 18:42:05 -0500

    cambios pequeños

--- src/routes/cliente.routes.ts
+++ src/routes/cliente.routes.ts
@@ -8,11 +8,11 @@ const router = Router();
 router.get('/list',passport.authenticate('jwt', { session: false }), getClientes);
 router.get('/:c_codigocompania/listXcompania',passport.authenticate('jwt', { session: false }), getClientesXCompania);
 router.get('/listAll',passport.authenticate('jwt', { session: false }), getClientesAdmin);
-router.get('/getClienteByCodigoCliente',passport.authenticate('jwt', { session: false }), getClienteByCodigoCliente);
+router.post('/getClienteByCodigoCliente',passport.authenticate('jwt', { session: false }), getClienteByCodigoCliente);
 router.put('/update',  passport.authenticate('jwt', { session: false }), updateCliente);
 router.post('/delete', passport.authenticate('jwt', { session: false }), deleteCliente );
 router.post('/register',passport.authenticate('jwt', { session: false }), registerCliente);
-router.get('/getClienteDinamico',getClienteDinamico);
+router.post('/getClienteDinamico',getClienteDinamico);
 
 
 export default router;
