482971f299c6d520ca8415ab7e77b1cf28a7aa98
Dennis Angeles <denisarmando203@gmail.com>
2021-12-27 15:06:32 -0500

    fixed untracked files

--- dist/interfaces/distrito.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=distrito.interface.js.map
--- dist/interfaces/provincia.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=provincia.interface.js.map
