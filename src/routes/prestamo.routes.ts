e20be820aa088a478d1c9c6ecb1263322aaaa116
unknown <carlitosangeles213@gmail.com>
2021-12-27 03:36:48 -0500

    Registrar y listar Préstamo

--- src/routes/prestamo.routes.ts
+++ src/routes/prestamo.routes.ts
@@ -1,18 +1,14 @@
 import { Router } from "express"
-import {registerPrestamo} from '../controllers/prestamo.controller'
+import {registerPrestamo,getPrestamoDinamico} from '../controllers/prestamo.controller'
 import passport from 'passport'
 
 
 const router = Router();
 /*
-router.get('/list',passport.authenticate('jwt', { session: false }), getClientes);
-router.get('/:c_codigocompania/listXcompania',passport.authenticate('jwt', { session: false }), getClientesXCompania);
-router.get('/listAll',passport.authenticate('jwt', { session: false }), getClientesAdmin);
-router.get('/getClienteByCodigoCliente',passport.authenticate('jwt', { session: false }), getClienteByCodigoCliente);
 router.put('/update',  passport.authenticate('jwt', { session: false }), updateCliente);
 router.post('/delete', passport.authenticate('jwt', { session: false }), deleteCliente );*/
 router.post('/register',passport.authenticate('jwt', { session: false }), registerPrestamo);
-//router.post('/getClienteDinamico',passport.authenticate('jwt', { session: false }),getClienteDinamico);
+router.post('/getPrestamoDinamico',passport.authenticate('jwt', { session: false }),getPrestamoDinamico);
 
 
 export default router;
