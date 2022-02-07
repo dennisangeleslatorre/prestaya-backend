68fdff3afce6d14eba912e935bfad3a8948d61d6
Dennis Angeles <denisarmando203@gmail.com>
2022-02-06 19:32:41 -0500

    Cambio env

--- src/database.ts
+++ src/database.ts
@@ -6,9 +6,9 @@ config()
 export async function connect() {
 
     const connection = await createPool({
-        host: process.env.HOST,
-        user: process.env.USER,
-        password: process.env.PASSWORD,
+        host: process.env.HOST_DB,
+        user: process.env.USER_DB,
+        password: process.env.PASSWORD_DB,
         database: process.env.DATABASE
 
     })
