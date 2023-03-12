7087a9854c73b82f8939c65d24d0fc1e5af1feaf
IiICarlosIiI <carlitosangeles213@gmail.com>
2023-03-12 17:44:59 -0500

    SERVICIO ANULAR TRANSACCION

--- src/routes/transaccion.routes.ts
+++ src/routes/transaccion.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express"
 
 import { getTransaccionDinamico } from '../controllers/transaccion.controller'
 import { getTransaccionDetalle } from '../controllers/transaccion.controller'
+import { updateTransaccionAnular } from '../controllers/transaccion.controller'
 
 import passport from 'passport'
 
@@ -9,5 +10,6 @@ const router = Router();
 
 router.post('/getTransaccionDinamico',passport.authenticate('jwt', { session: false }), getTransaccionDinamico);
 router.post('/getTransaccionDetalle',passport.authenticate('jwt', { session: false }), getTransaccionDetalle);
+router.put('/updateTransaccionAnular',passport.authenticate('jwt', { session: false }),updateTransaccionAnular);
 
 export default router;
