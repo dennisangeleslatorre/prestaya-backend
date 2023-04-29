425900e730bf6f51377e33d4ae069d20f81a816d
Dennis Angeles <denisarmando203@gmail.com>
2023-04-29 01:45:03 -0500

    Servicios de lista de ubicaciones

--- src/controllers/ubicacion.controller.ts
+++ src/controllers/ubicacion.controller.ts
@@ -47,6 +47,46 @@ export async function getAgenciaUbicacion(req: Request, res: Response): Promise<
     }
 }
 
+export async function getAllAgenciaUbicacion(req: Request, res: Response): Promise<Response> {
+    try {
+            const conn = await connect();
+            const [rows, fields] = await conn.query(
+                `SELECT mc.c_descripcion as compania_desc, ma.c_descripcion as agencia_desc, mu.*
+                FROM prestaya.ma_ubicacionagencia mu
+                INNER JOIN ma_agencia ma ON mu.c_compania=ma.c_compania AND mu.c_agencia=ma.c_agencia
+                INNER JOIN ma_compania mc ON ma.c_compania=mc.c_compania`)
+            await conn.end();
+            const ubicacionagenciaRes =rows as [UbicacionAgencia];
+            if(!ubicacionagenciaRes[0]) {
+                return res.status(200).json({ data:[], message: "No se encontró ubiación para esa agencia" });
+            }
+            return res.status(200).json({ data:rows, message: "Se obtuvo registros" });
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send(error)
+    }
+}
+
+export async function getAgenciaUbicacionByCodigo(req: Request, res: Response): Promise<Response> {
+    try {
+        const body = req.body;
+        const ubicacionagencia: UbicacionAgencia = body;
+        if(ubicacionagencia.c_compania && ubicacionagencia.c_agencia ) {
+            const conn = await connect();
+            const [rows, fields] = await conn.query('SELECT * FROM prestaya.ma_ubicacionagencia where c_compania=? AND c_agencia=?',[ubicacionagencia.c_compania,ubicacionagencia.c_agencia])
+            await conn.end();
+            const ubicacionagenciaRes =rows as [UbicacionAgencia];
+            if(!ubicacionagenciaRes[0]) {
+                return res.status(200).json({ data:[], message: "No se encontró ubiación para esa agencia" });
+            }
+            return res.status(200).json({ data:rows, message: "Se obtuvo registros" });
+        }return res.status(200).json({ message: "Se debe enviar la compañía y agencia para listar la ubicación" });
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send(error)
+    }
+}
+
 
 export async function updateUbicacionAgencia(req: Request, res: Response): Promise<Response> {
     try {
--- src/routes/ubicacion.routes.ts
+++ src/routes/ubicacion.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express"
-import { registerUbicacionAgencia, getAgenciaUbicacion, updateUbicacionAgencia, deleteUbiacionAgencia} from '../controllers/ubicacion.controller'
+import { registerUbicacionAgencia, getAgenciaUbicacion, updateUbicacionAgencia,
+        deleteUbiacionAgencia, getAllAgenciaUbicacion, getAgenciaUbicacionByCodigo} from '../controllers/ubicacion.controller'
 import passport from 'passport'
 
 const router = Router();
 
 router.post('/registerUbicacion',passport.authenticate('jwt', { session: false }), registerUbicacionAgencia);
-router.post('/listUbicacion',passport.authenticate('jwt', { session: false }), getAgenciaUbicacion);
+router.post('/listUbicacionesByCodigo',passport.authenticate('jwt', { session: false }), getAgenciaUbicacion);
+router.post('/getAgenciaUbicacionByCodigo',passport.authenticate('jwt', { session: false }), getAgenciaUbicacionByCodigo);
+router.post('/listAll',passport.authenticate('jwt', { session: false }), getAllAgenciaUbicacion);
 router.put('/updateUbicacion',  passport.authenticate('jwt', { session: false }), updateUbicacionAgencia);
 router.post('/deleteUbicacion', passport.authenticate('jwt', { session: false }), deleteUbiacionAgencia );
 
